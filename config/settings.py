4bbbe4eb4af105df0a7aaeac10c9f357ae3923ad
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-07-24 16:06:40 +0300

    chore: configure drf authentication and jwt

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +134,27 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+
+     "DEFAULT_AUTHENTICATION_CLASSES" : (
+
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+
+     ),
+
+     "DEFAULT_PERMISSION_CLASSES": (
+
+         "rest_framework.permissions.IsAuthenticated",
+     ),
+
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes =15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days = 7)
+}
+
+
+
