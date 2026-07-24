e790edf490d750796ef244f730b8d5ed02d5503e
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-07-24 15:52:02 +0300

    chore: update admin panels

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
 
-# Register your models here.
+from finance.models.category import Category
+from finance.models.transaction import Transaction
+from finance.models.wallet import Wallet
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "user",
+        "balance",
+        "currency",
+    )
+    search_fields = (
+        "name",
+        "user__username",
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "user",
+        "type",
+    )
+    search_fields = (
+        "name",
+        "user__username",
+    )
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "wallet",
+        "category",
+        "amount",
+        "transaction_date",
+    )
+    list_filter = (
+        "category__type",
+        "transaction_date",
+    )
