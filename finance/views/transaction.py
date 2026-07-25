c242f1cce792e7444457b0765119cb927174f109
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-07-25 13:03:05 +0300

    feat: implement transaction creation logic

--- finance/views/transaction.py
+++ finance/views/transaction.py
@@ -1,8 +1,8 @@
 from finance.serializers.transaction import TransactionSerializer
 from rest_framework.viewsets import ModelViewSet
 from finance.models.transaction import Transaction
-
-
+from django.db import transaction
+from finance.models.choices import CategoryType
 
 class TransactionViewSet(ModelViewSet):
 
@@ -12,5 +12,24 @@ class TransactionViewSet(ModelViewSet):
 
     def get_queryset(self):
 
-       return Transaction.objects.filter(wallet__user = self.request.user)
+       return Transaction.objects.filter(wallet__user = self.request.user).select_related("wallet", "category")
+
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            transaction_obj = serializer.save()
+
+            wallet = transaction_obj.wallet
+
+            category = transaction_obj.category
+
+            if category.type == CategoryType.INCOME:
+
+                wallet.balance += transaction_obj.amount
+
+            elif category.type == CategoryType.EXPENSE:
+
+                wallet.balance -= transaction_obj.amount
+
+            wallet.save()
+
 
