a5db5fb83c36a660d6e977ae4492ff8529d47693
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-07-25 18:10:51 +0300

    feat: add transfer serializer

---
+++ finance/serializers/transfer.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+
+from finance.models.transfer import Transfer
+
+
+class TransferSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Transfer
+
+        fields = [
+            "id",
+            "from_wallet",
+            "to_wallet",
+            "amount",
+            "description",
+            "transfer_date",
+            "created_at",
+            "updated_at",
+        ]
+
+        read_only_fields = [
+            "id",
+            "created_at",
+            "updated_at",
+        ]
+
+    def validate_from_wallet(self, wallet):
+        if wallet.user != self.context["request"].user:
+            raise serializers.ValidationError(
+                "You don't have access to this wallet."
+            )
+        return wallet
+
+    def validate_to_wallet(self, wallet):
+        if wallet.user != self.context["request"].user:
+            raise serializers.ValidationError(
+                "You don't have access to this wallet."
+            )
+        return wallet
+
+    def validate(self, attrs):
+        from_wallet = attrs["from_wallet"]
+        to_wallet = attrs["to_wallet"]
+        amount = attrs["amount"]
+
+        if from_wallet == to_wallet:
+            raise serializers.ValidationError(
+                "Source and destination wallets must be different."
+            )
+
+        if from_wallet.balance < amount:
+            raise serializers.ValidationError(
+                "Insufficient funds."
+            )
+
+        return attrs
