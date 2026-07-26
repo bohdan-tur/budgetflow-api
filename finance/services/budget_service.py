fe9d6d8f9ffcca3d1eb57c31ad54220fefe393b0
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-07-26 13:53:54 +0300

    feat: implement budget statistics calculations

--- finance/services/budget_service.py
+++ finance/services/budget_service.py
@@ -1,8 +1,14 @@
-from typing import Any
 
+from typing import Any
 from django.contrib.auth import get_user_model
 from django.db import transaction
+from decimal import Decimal
 
+from django.db.models import DecimalField, Sum
+from django.db.models.functions import Coalesce
+
+from finance.models.choices import CategoryType
+from finance.models.transaction import Transaction
 from finance.models.budget import Budget
 
 User = get_user_model()
@@ -47,3 +53,56 @@ class BudgetService:
         instance: Budget,
     ) -> None:
         instance.delete()
+
+    @staticmethod
+    def get_spent_amount(*, budget: Budget) -> Decimal:
+        result = (
+            Transaction.objects.filter(
+                wallet__user=budget.user,
+                category=budget.category,
+                transaction_date__range=(
+                    budget.start_date,
+                    budget.end_date,
+                ),
+                category__type=CategoryType.EXPENSE,
+            )
+            .aggregate(
+                total=Coalesce(
+                    Sum("amount"),
+                    Decimal("0.00"),
+                    output_field=DecimalField(),
+                )
+            )
+        )
+
+        return result["total"]
+
+    @staticmethod
+    def get_remaining_amount(*, budget: Budget) -> Decimal:
+        spent = BudgetService.get_spent_amount(
+            budget=budget,
+        )
+
+        return budget.amount - spent
+
+    @staticmethod
+    def get_percentage_used(*, budget: Budget) -> Decimal:
+        if budget.amount == Decimal("0.00"):
+            return Decimal("0.00")
+
+        spent = BudgetService.get_spent_amount(
+            budget=budget,
+        )
+
+        return (
+                (spent / budget.amount) * Decimal("100")
+        ).quantize(Decimal("0.01"))
+
+    @staticmethod
+    def is_budget_exceeded(*, budget: Budget) -> bool:
+        spent = BudgetService.get_spent_amount(
+            budget=budget,
+        )
+
+        return spent > budget.amount
+
