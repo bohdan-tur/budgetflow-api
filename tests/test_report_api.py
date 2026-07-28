8ca17bb5af8022281389ba6c648a9f03c0e5c2a0
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-07-28 22:52:34 +0300

    test: add report api tests

---
+++ tests/test_report_api.py
@@ -0,0 +1,40 @@
+from decimal import Decimal
+
+import pytest
+from rest_framework import status
+
+from tests.factories import WalletFactory, TransactionFactory
+
+pytestmark = pytest.mark.django_db
+
+
+def test_get_monthly_report(auth_client,user,income_category,expense_category):
+    wallet = WalletFactory(user=user)
+
+    TransactionFactory(
+        wallet=wallet,
+        category=income_category,
+        amount=100,
+    )
+
+    TransactionFactory(
+        wallet=wallet,
+        category=expense_category,
+        amount=50,
+    )
+    response = auth_client.get("/api/reports/monthly_statistics/")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.data) == 1
+
+    report = response.data[0]
+
+    assert   report['income'] == "100.00"
+    assert   report['expense'] == "50.00"
+
+
+
+def test_get_monthly_report_unauthorized(api_client):
+    response = api_client.get("/api/reports/monthly_statistics/")
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
