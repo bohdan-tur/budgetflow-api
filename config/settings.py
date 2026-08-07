eeaec62c4497e972dcaad472b23600f98641f19f
Tyrevich Bohdan <turevicbogdan4@gmail.com>
2026-08-07 15:23:46 +0300

    config: make JWT lifetimes environment-driven

--- config/settings.py
+++ config/settings.py
@@ -149,6 +149,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(
+        minutes=int(os.getenv("ACCESS_TOKEN_MINUTES", 15))
+    ),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=int(os.getenv("REFRESH_TOKEN_DAYS", 7))),
 }
